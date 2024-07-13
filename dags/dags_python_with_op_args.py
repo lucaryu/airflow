5ac8a10c2c66bf756a1c8987dbe2a00d31d655d1
dhyou <polo83you@gmail.com>
2024-07-13 14:56:14 +0900

    수정

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 import datetime
-import airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
